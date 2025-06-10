fa3180e124d5a8d1b624d124f38edd4ca741bbc0
Pedro Gregorio <pedro-gregorio@outlook.pt>
2025-06-10 22:37:42 +0100

    Add: Themes Colors and StackComponent

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,22 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
-
-import "../global.css";
+import { View, Text, Image, useColorScheme } from "react-native";
 import { Link } from "expo-router";
 
+import { Colors } from "../constants/Colors";
+import "../global.css";
+
 const Home = () => {
+  const colorScheme = useColorScheme() || "light";
+  const theme = Colors[colorScheme];
+
   return (
-    <View className="container">
+    <View className="container" style={{ backgroundColor: theme.background }}>
       <Image
         className="my-4"
         source={require("../assets/img/logo_light.png")}
       />
 
       <Text className="title">Hello World</Text>
-      <Text className="mt-2 mb-6">Hello World 2</Text>
+      <Text className="mt-2 mb-6">Welcome to Shelfie.</Text>
 
       <View className="card">
         <Text>Hello, this is a card.</Text>
